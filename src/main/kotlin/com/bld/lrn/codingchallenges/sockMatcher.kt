96ec12b7d912107d41024c57aea93845fa801639
Benjamin Douthwaite <benjamin.douthwaite@booking.com>
2022-05-13 16:04:33 +0100

    Add Toms implementation, some extra tests, and comment for alternative implementation

--- src/main/kotlin/com/bld/lrn/codingchallenges/sockMatcher.kt
+++ src/main/kotlin/com/bld/lrn/codingchallenges/sockMatcher.kt
@@ -1,9 +1,34 @@
 package com.bld.lrn.codingchallenges
 
-fun sockMatcher(n: Array<Int>): Int {
+fun sockMatcher(arrayOfSockColours: Array<Int>): Int {
     
-    println("Sock matcher challenge - test")
     
-    return 1
+    
+    // Create a map of the colours of each sock, and initialise the values.
+    val mapOfColours: MutableMap<Int, Int> = mutableMapOf()
+    for (element in arrayOfSockColours) {
+        if (!mapOfColours.containsKey(element)) {
+            mapOfColours[element] = 1
+        } else {
+            mapOfColours[element] = mapOfColours[element]!! + 1
+        }
+    }
+    println(mapOfColours)
+    
+    // todo - kotlin has a lot of built in functions. Can we do the above ^ any easier? ( hint:
+    //  myCountingMap = myInputList.groupingBy { it }
+    
+    // Given our map values, count the number of matching pairs.
+    
+    var firstCount = mapOfColours[1]
+    var secondCount = mapOfColours[2]
+    var thirdCount = mapOfColours[3]
+    
+    var numOfPairs = firstCount?.div(2)
+    var numOfPairs2 = secondCount?.div(2)
+    var numOfPairs3 = thirdCount?.div(2)
+    
+    
+    return numOfPairs!! + numOfPairs2!! + numOfPairs3!!
     
 }
--- src/test/kotlin/com/bld/lrn/codingchallenges/SockMatcherKtTest.kt
+++ src/test/kotlin/com/bld/lrn/codingchallenges/SockMatcherKtTest.kt
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test
 internal class SockMatcherKtTest {
     
     @Test
-    fun sockMatcher() {
+    fun sockMatcher_descriptionCase() {
         // given
         val input = arrayOf(1,2,1,2,1,3,2)
     
@@ -16,5 +16,41 @@ internal class SockMatcherKtTest {
         
         // then
         assertEquals(2, result)
+    }
+    
+    @Test
+    fun sockMatcher_allMatching() {
+        // given
+        val input = arrayOf(5,5,5,5,5,5)
+        
+        // when
+        val result = sockMatcher(input)
+        
+        // then
+        assertEquals(3, result)
+    }
+    
+    @Test
+    fun sockMatcher_noneMatching() {
+        // given
+        val input = arrayOf(1,2,3,4,5,6)
+        
+        // when
+        val result = sockMatcher(input)
+        
+        // then
+        assertEquals(0, result)
+    }
+    
+    @Test
+    fun sockMatcher_allPairs_highNumbers() {
+        // given
+        val input = arrayOf(100,200,300,100,200,300)
+        
+        // when
+        val result = sockMatcher(input)
+        
+        // then
+        assertEquals(3, result)
     }
 }
